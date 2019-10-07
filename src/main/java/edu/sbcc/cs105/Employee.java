00200ba5bebd220a5d037bb368936199d1af8712
IanMcGowan62 <ianpatmcgowan@gmail.com>
2019-10-06 21:45:31 -0700

    Done

---
+++ src/main/java/edu/sbcc/cs105/Employee.java
@@ -0,0 +1,55 @@
+/**
+ * CS 105 Theory and Practice I
+ * CRN: 38065
+ * Assignment: Employee
+ * 
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ * 
+ * @author Ian McGowan
+ */
+package edu.sbcc.cs105;
+
+/**
+ * This class implements an emplyee which is a person with a name and a salary.
+ *
+ */
+public class Employee {
+
+    /**
+     * Constructor that creates a new Employee with an initial name and salary.
+     * 
+     */
+    String employeeName;
+    double currentSalary;
+
+
+    public Employee(String employeeName, double currentSalary) {
+        // TODO: Initialize instance variables from constructor (ctor) parameters
+        this.employeeName = employeeName;
+        this.currentSalary = currentSalary;
+    }
+
+    // Accessors that are obvious and have no side effects don't have to have
+    // any documentation unless you are creating a library to be used by other
+    // people.
+    public String getName() {
+        // TODO: Return the name of the employee;
+        return employeeName;
+    }
+
+    public double getSalary() {
+        // TODO: Return the current salary of the employee
+        return currentSalary;
+    }
+
+    /**
+     * Raise the salary by the amount specified by the explicit argument.
+     * 
+     */
+    public void raiseSalary(double byPercent) {
+        double addToAmount = currentSalary * (byPercent/100);
+        
+        currentSalary = currentSalary + addToAmount;
+    }
+}
